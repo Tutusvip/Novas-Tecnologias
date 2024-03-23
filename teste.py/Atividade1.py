18a655b4c62398bcaff3fa3e58f386c22fd07137
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2024-03-22 21:06:05 -0300

    Add files via upload

---
+++ teste.py/Atividade1.py
@@ -0,0 +1,11 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1= int(input("Digite um numero: "))
+... num2= int(input("Digite outro numero: "))
+... 
+... print(f"{num1} + {num2} = {num1+num2}")
+... print(f"{num1} + {num2} = {num1*num2}")
+... print(f"{num1} + {num2} = {num1/num2}")
+... print(f"{num1} + {num2} = {num1-7.num2}")
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
